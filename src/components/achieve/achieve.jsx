63ae55716e430ca13d45337791d1aca7ce11b99e
hamzaaldirawi <hamza.dirawie@gmail.com>
2021-02-12 16:17:17 +0300

    style

--- src/components/achieve/achieve.jsx
+++ src/components/achieve/achieve.jsx
@@ -2,31 +2,33 @@ import { FaMoneyBill } from "@react-icons/all-files/fa/FaMoneyBill";
 import { FaThumbtack } from "@react-icons/all-files/fa/FaThumbtack";
 import { FaSpinner } from "@react-icons/all-files/fa/FaSpinner";
 
-import { Container } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
+
+import './achieve.scss';
 
 const Acheive = ({users}) => {
     const {totalEarn, sold, in_progress}= users.data;
     
     return (
-        <Container>
+        <Navbar>
         <div className="achieve">
-            <span> 
+            <span className="achieve-span"> 
                 <FaMoneyBill />
                 <p>Total Earn: {totalEarn} $</p>
             </span> 
 
-            <span>
+            <span className="achieve-span">
                 <FaThumbtack />
                 <p>Order Sold: {sold}</p>
             </span>
 
-            <span>
+            <span className="achieve-span">
                 <FaSpinner />
                 <p>Order in progress: {in_progress}</p>
             </span>
 
         </div>
-        </Container>
+        </Navbar>
 
     );
 }
